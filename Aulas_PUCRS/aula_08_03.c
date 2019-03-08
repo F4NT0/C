e1a7b1e57fba308e78d631c379f5fc3a090e02e3
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-03-08 20:52:34 -0300

    aula 08/03 3

--- Aulas_PUCRS/aula_08_03.c
+++ Aulas_PUCRS/aula_08_03.c
@@ -12,14 +12,22 @@
 // etapa 4: 
 
 #include <stdio.h> //biblioteca I/O
+#include <math.h> // biblioteca matematica
 
 int main(){
+    //variaveis
     int a,b,c,d;
+
+    // iniciado um vetor
+    double vetor[10];
+
+
     printf("Informe dois numeros: ");
     scanf("%d %d", &a, &b);
 
     printf("a = %d, b = %d\n",a,b);
 
+    // IF/ELSE EM C
     if(0 < a <= 10){ // na linguagem Java esta instrução não compila
         printf("a se encontra entre 0 e 10\n");
     }
@@ -39,9 +47,34 @@ int main(){
 
     printf("c= %d e d= %d \n",c,d);
 
+    //WHILE EM C
     while(c < d){
         printf("c= %d\n",c);
         c++;
+    }
+
+    // FOR EM C
+    for(int i = a ; i < b ; i++){
+        printf("i = %d\n", i);
+        a++;
+    }
+
+    //aberração de um for
+    //for(;;){
+    //    printf("i = %d\n",a);
+    //    a++;
+    //    if (a == b) break;
+    //}
+
+    // MEXENDO COM VETORES E ADICIONANDO VALORES
+
+    // para adicionar a biblioteca de matematica no gcc, pq deu erro
+    // gcc nomeprograma -o nomecompilador -lm 
+
+
+    for(int i = 0 ; i < 10 ; i++){
+        vetor[i] = pow(2,i);
+        printf("[%d]: %f\n",i, vetor[i]);
     }
 
     return 0;
